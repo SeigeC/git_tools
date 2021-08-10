5d165c7793c2a2d0fbc2bbaca691ea10da9c69a7
486315944@qq.com <486315944@qq.com>
2021-08-10 14:56:01 +0800

    初步接入 graphql，通过 graphql 准确获取 approve 信息

--- func/pr.ts
+++ func/pr.ts
@@ -1,14 +1,16 @@
 import json from '../github_user.json'
-const { userList, projectList } = json
-import { GetMyPr } from '../common/pr.js'
+import {GetApprovedUser, GetMyPr} from '../common/pr.js'
 import prompts from 'prompts';
+
+const {userList, projectList} = json
+
 export const description = '催 Pr'
 export default async function Pr() {
     const project = (await prompts({
         type: 'select',
         name: 'value',
         message: '请选择要拉取的项目',
-        choices: Object.keys(projectList).map(item => ({ title: item, value: item })),
+        choices: Object.keys(projectList).map(item => ({title: item, value: item})),
         initial: 0
     })).value
 
@@ -19,37 +21,53 @@ export default async function Pr() {
         console.error("res 不能为空")
         await $`exit`
     }
-    // console.log(JSON.stringify(res));
-    // return
+
     const wantReviewList = (await prompts({
         type: 'multiselect',
         name: 'value',
         message: '请选择要催的 PR',
-        choices: res.map(item => ({ title: item.title, value: item.number })),
+        choices: res.map(item => ({title: item.title, value: item.number})),
         min: 1,
         initial: 0
-    })).value
+    })).value as string[]
 
     res = res.filter(item => wantReviewList.includes(item.number))
 
-    function needReview(pr, name) {
+    function needReview(approved_users: string[], name: string) {
+        if (approved_users.includes(name)) {
+            return false
+        }
         const review_user = userList[name]
         if (!review_user?.phone) {
-            return
+            return false
         }
-        const rule = review_user.reivew_rule
+        const rule = review_user.reivew_rule as { and: string[], or: string[] }
         if (!rule) {
-            return pr.requested_reviewers.find((user) => user.login === name) !== undefined
+            return true
         }
-        const requested_reviewers_names = pr.requested_reviewers.map(user => user.login)
         if (rule.and) {
-            return rule.and.every(name => requested_reviewers_names.includes(name))
+            return rule.and.every(name => approved_users.includes(name))
         }
         // or 
-        return !rule.or.every(name => requested_reviewers_names.includes(name))
+        return !!rule.or.find(name => approved_users.includes(name))
     }
 
-    const pushMap = new Map()
+    async function setPusMap(item) {
+        const approved_users = await GetApprovedUser({
+            project_name: project, number: parseInt(item.number)
+        })
+
+        const not_approved_users = wantReviewUser.filter(user => !approved_users.includes(user))
+        not_approved_users.forEach(name => {
+            if (!needReview(approved_users, name)) {
+                return
+            }
+            const list = pushMap.get(name)
+            list ? pushMap.get(name).push(item) : pushMap.set(name, [item])
+        })
+        return
+    }
+    
     if (!projectList[project]?.review_userList) {
         return
     }
@@ -57,16 +75,14 @@ export default async function Pr() {
     const wantReviewUser = (await prompts({
         type: 'multiselect',
         name: 'value',
-        message: '请选择要催的 PR',
-        choices: projectList[project].review_userList.map(item => ({ title: item, value: item })),
+        message: '请选择要 review 的人',
+        choices: projectList[project].review_userList.map(item => ({title: item, value: item})),
         min: 1,
         initial: 0
-    })).value
-    wantReviewUser.forEach(name => {
-        const list = res.filter(item => needReview(item, name))
-        list.length <= 0 ? null : pushMap.set(name, list)
-    })
+    })).value as string[]
 
+    const pushMap = new Map()
+    await Promise.all(res.map((item: any) => setPusMap(item)))
     if (pushMap.size === 0) {
         return
     }
@@ -88,7 +104,7 @@ export default async function Pr() {
                 "mentioned_mobile_list": phonelist
             }
         }),
-        headers: { 'Content-Type': 'application/json' },
+        headers: {'Content-Type': 'application/json'},
     })
     console.log(await bot.json())
 }
