735e5767650d2615053e77ed4d837ba6e84629ee
486315944@qq.com <486315944@qq.com>
2021-08-11 16:54:47 +0800

    支持直接使用

--- common/store.ts
+++ common/store.ts
@@ -5,13 +5,15 @@ import type {config as IConfig} from './config.js'
 const __filename = process.argv[2]
 const __dirname = path.dirname(__filename);
 const __config_path = `${__dirname}/config`
-const __swimlane_path = `${__config_path}/swimlanepath`
+const __swimlane_path = `${__config_path}/swimlane`
 
 export {__filename, __dirname, __config_path, __swimlane_path}
 
 export async function init() {
     const config: IConfig = {}
-    config.swimlane = readdirSync(__swimlane_path).filter(file => file.endsWith(".yml") && !file.endsWith(".example.yml"))
+    config.swimlane = fs.existsSync(__swimlane_path) ?
+        readdirSync(__swimlane_path).filter(file => file.endsWith(".yml") && !file.endsWith(".example.yml")) :
+        []
     const list = readdirSync(__config_path).filter(file => file.endsWith(".json") && !file.endsWith(".example.json"))
     await Promise.all(
         list.map(async file => {
