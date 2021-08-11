9efdc1349dbda29ae2225354ad65dbe9bf5e74bf
过客龙门 <486315944@qq.com>
2021-08-11 16:56:13 +0800

    支持直接使用 (#5)

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,17 +1,15 @@
 import esbuild from "esbuild";
 // Automatically exclude all node_modules from the bundled version
-import {nodeExternalsPlugin} from 'esbuild-node-externals'
 import ImportGlobPlugin from 'esbuild-plugin-import-glob'
 
 esbuild.build({
     entryPoints: ['./index.ts'],
     outfile: './index.js',
     bundle: true,
-    // minify: true,
+    minify: true,
     platform: 'node',
-    sourcemap: true,
     format: "esm",
-    plugins: [ImportGlobPlugin.default(), nodeExternalsPlugin()]
+    plugins: [ImportGlobPlugin.default()]
 }).catch(err => {
     console.log(err)
     process.exit(1)
